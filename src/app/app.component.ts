6eed1f739881eed360eee25b72ec030af80fdea5
Dust <dearestdust071@gmail.com>
2025-02-16 21:23:34 -0600

    Commit inicial código base de clase

---
+++ src/app/app.component.ts
@@ -0,0 +1,85 @@
+import { Component, inject, Input } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { WebsocketsServiceService } from './services/websockets.service.service';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+import { CommonModule, NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  host: {ngSkipHydration: 'true'},
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule, NgFor, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'], // Cambiado a styleUrls
+})
+export class AppComponent {
+  private formBuilder: FormBuilder = inject(FormBuilder); //Inyectamos formbuilder
+  private ws: WebsocketsServiceService = inject(WebsocketsServiceService); //Inyectamos servicio
+
+  formulario: FormGroup = this.formBuilder.group({ //caja de texto formulario de rooms
+    msg: new FormControl(''),
+  });
+
+  formulario3: FormGroup = this.formBuilder.group({ //caja de texto formulario de rooms
+    msg: new FormControl(''),
+  }); 
+
+  formulario2: FormGroup = this.formBuilder.group({ 
+    rooms: new FormControl([]),
+  }); 
+
+  title = 'prueba';
+  roomNameStored = '';
+  roomAvailable:any;
+  eventName = '';
+
+  async ngAfterViewInit() {
+    this.ListenMessageForRoom();
+    this.ListenFormChanges();
+    this.ListenAvailableRooms();
+  }
+
+  ListenFormChanges(){
+    //escuchar los cambios del formulario
+    this.formulario.get('msg')?.valueChanges.pipe(
+      debounceTime(500)
+    ).subscribe(value => {
+      this.ws.EmitToRoom(this.roomNameStored, this.formulario.value.msg);
+    });
+  }
+
+  // Listen() { //Escucha los mensajes del socket
+  //   this.ws.observable.subscribe((msg: string) => {
+  //     this.formulario.get('msg')?.patchValue(msg, { emitEvent: false });
+  //   });
+  // }
+
+  ListenAvailableRooms() { //Escucha las rooms disponibles (tarea)
+    this.ws.Listen('rooms').subscribe((ListOfRooms: any) => {
+      console.log(ListOfRooms);
+      // this.formulario2.get('rooms')?.patchValue(room, { emitEvent: false });
+      this.roomAvailable = ListOfRooms;
+    });
+  }
+
+  ListenMessageForRoom() { //Escucha los mensajes de las rooms
+    this.ws.Listen('message').subscribe((msg: string) => {
+      // En de abajo se cambia para parchar un formulario o espacio para poner la informacion necesaria
+      console.log(msg);
+      this.formulario.get('msg')?.patchValue(msg, { emitEvent: false });
+    });
+  }
+
+  JoinRoom(roomName: string){
+    if(this.roomNameStored != ''){
+      this.ws.LeaveRoom(this.roomNameStored);
+    }
+    this.roomNameStored = roomName; //Actualiza la room a la que declaramos en el front
+    this.ws.JoinRoom(roomName); //nos unimos a la room especificada
+    this.ListenMessageForRoom();//llamamos el metodo listen room para empezar a escuhar a la nueva sala
+    // this.ListenRoo();//llamamos el metodo listen room para empezar a escuhar a la nueva sala
+    this.ListenFormChanges(); //escucha los cambios en los otros formularios
+    this.formulario3.reset(); //resetea el input
+  }
+}
---
+++ src/app/services/websockets.service.service.ts
@@ -0,0 +1,164 @@
+import { isPlatformBrowser } from '@angular/common';
+import { Injectable, PLATFORM_ID, inject, } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+
+
+export interface EntryModel { //Recibe el modelo que mandamos desde .NET (modelo, sockets)
+  Action_Type: string;
+  Value: {
+    Name: string;
+    Msg: string;
+    Rooms: [Room]
+
+  };
+}
+
+export interface Room { //Recibe el modelo que mandamos desde .NET (modelo, sockets)
+  RoomName: string;
+    User: string;
+  };
+
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WebsocketsServiceService {
+  
+  private socket!: WebSocket; //Abre la conexion
+  private socketOpenPromise: Promise<void>; //Checar que ya esté conectado
+  public observable: Subject<EntryModel> = new Subject<EntryModel>(); //Recibir y vernlos mensajes
+  private platform_id: Object = inject(PLATFORM_ID);
+
+
+  constructor() {
+    //Abre la conexion
+    if (isPlatformBrowser(this.platform_id) && typeof window != undefined)
+      this.socket = new WebSocket('ws://192.168.30.155:9001');
+
+
+    this.socketOpenPromise = new Promise<void>((resolve, reject) => {
+
+      if (this.socket != undefined) {
+        this.socket.onopen = () => {
+          console.log('Connect');
+          resolve();
+        };
+        this.socket.onerror = (event) => {
+          reject(event);
+        };
+      }
+    });
+
+    if (this.socket != undefined) {
+      this.socket.onclose = () => {
+        console.log('Disconnect');
+      };
+    }
+
+    if (this.socket != undefined) {
+      this.socket.onmessage = (event) => {
+        console.log(event.data);
+        
+        try {
+          const jsonObject: EntryModel = JSON.parse(event.data);
+          console.log(jsonObject);
+          this.observable.next(jsonObject);
+          
+        } catch (error) {
+          console.error('Error parsing JSON:', error);
+        }
+      };
+    }
+
+  }
+
+  public async Emit(content: string) {
+    try {
+      await this.socketOpenPromise;
+      this.socket.send(content);
+    } catch (error) {
+      console.error('Error connecting', error);
+    }
+  }
+
+
+
+
+  public Listen(eventName:string): Observable<any> { //Escucha los mensajes que vienen del websocket, filtrando los mensajes dependiendo su ActionType
+    return new Observable<any>((observer) => {
+      this.observable.subscribe((entry: EntryModel) => {
+        switch (entry.Action_Type) {
+          case 'message':
+            if (entry.Action_Type === eventName) {
+              observer.next(entry.Value.Msg)
+            }
+            // observer.next(entry.Value.Rooms);
+            break;
+            case 'rooms':
+            if (entry.Action_Type === eventName) {
+              observer.next(entry.Value.Rooms);
+            }
+            break;
+          default:
+            console.error("error");
+            break;
+        }
+      });
+    });
+  }
+
+  public async JoinRoom(roomName: string) {
+    try {
+      await this.socketOpenPromise;
+      const message = {
+        Action_Type: 'join',
+        Value: {
+          Name: roomName,
+          Msg: ''
+        },
+      };
+      this.socket.send(JSON.stringify(message));
+    } catch (error) {
+      console.error('Error connecting to the WebSocket:', error);
+    }
+  }
+
+  public async LeaveRoom(roomName: string) {
+    try {
+      await this.socketOpenPromise;
+      const message = {
+        Action_Type: 'leave',
+        Value: {
+          Name: roomName,
+          Msg: ''
+        },
+      };
+      this.socket.send(JSON.stringify(message));
+    } catch (error) {
+      console.error('Error connecting to the WebSocket:', error);
+    }
+  }
+  
+  public async EmitToRoom(roomName: string, content: string) {
+    try {
+      await this.socketOpenPromise;
+      const message = {
+        Action_Type: 'message',
+        Value: {
+          Name: roomName,
+          Msg: content
+        },
+      };
+      this.socket.send(JSON.stringify(message));
+    } catch (error) {
+      console.error('Error connecting', error);
+    }
+  }
+
+
+  // Tarea: Cuando ponga crear o join tiene que sacarme de donde ando y meterme en la nueva sala (Obligatoria)
+  // Tarea2: Que vean la forma de que me aparezca una lista de las rooms que estan disponibles 
+
+}
